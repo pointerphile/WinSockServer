a4b6d12e0902cf1a8409a3f00ea596b575643ff8
*p <pointerphile@gmail.com>
2018-06-25 05:08:09 +0900

    test

--- Source/WinSockServerTCP/main.cpp
+++ Source/WinSockServerTCP/main.cpp
@@ -3,6 +3,8 @@
 #include <WinSock2.h>
 #pragma comment(lib, "ws2_32.lib")
 
+#define SERVER_PORT 10000
+
 int main(int argc, char* argv[]) {
 	std::cout << "PPServer" << std::endl;
 
@@ -15,12 +17,15 @@ int main(int argc, char* argv[]) {
 
 	SOCKET socketListen;
 	socketListen = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketListen == INVALID_SOCKET) {
+		return -1;
+	}
 
 	SOCKADDR_IN saServer;
 	saServer = { 0 };
 	saServer.sin_family = AF_INET;
-	saServer.sin_port = htons(10000);// host byte 정렬 to network byte 정렬 short
-	saServer.sin_addr.s_addr = inet_addr("127.0.0.1");
+	saServer.sin_port = htons(SERVER_PORT);
+	saServer.sin_addr.s_addr = htonl(INADDR_ANY);
 	iRet = bind(socketListen, (SOCKADDR*)&saServer, sizeof(saServer));
 	if (iRet == SOCKET_ERROR) {
 		return -1;
@@ -35,6 +40,8 @@ int main(int argc, char* argv[]) {
 
 	SOCKET socketClient;
 	SOCKADDR_IN saClient;
+	char buf[256] = { 0 };
+	int iLen;
 	
 	while(1){
 		int iAddLen = sizeof(saClient);
@@ -45,9 +52,6 @@ int main(int argc, char* argv[]) {
 		std::cout << std::endl;
 		std::cout << "접속자 IP : " << inet_ntoa(saClient.sin_addr) 
 			<< " : " << ntohs(saClient.sin_port) << std::endl;
-
-		char buf[256] = { 0 };
-		int iLen;
 
 		closesocket(socketClient);
 	}
